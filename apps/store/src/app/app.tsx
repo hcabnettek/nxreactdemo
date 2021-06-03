6740229f422ab26a7cb81f9b3d5fbe19e76d3a75
Chris Kettenbach <295833+hcabnettek@users.noreply.github.com>
2021-06-03 14:36:52 -0700

    added express api

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @nrwl/nx/enforce-module-boundaries */
 import './app.scss';
 import { getAllGames } from '../fake-api';
@@ -11,59 +12,76 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
 import { Route, useHistory } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
-  const [query, setQuery] = useState('4mpr1usekm72');
+  const [query, setQuery] = useState('ngs2yy0u3tkh');
   const url =
     query && `https://deckofcardsapi.com/api/deck/${query}/draw/?count=2`;
   const { status, data } = useFetch(url);
   const cards = data?.cards;
   const history = useHistory();
-  console.log(cards);
+
+  const [q2, setQ2] = useState('ngs2yy0u3tkh');
+  const url2 = q2 && `/api/games`;
+  const { status: s2, data: d2 } = useFetch(url2);
+  //: {value: string; suit: string; images: {svg: string;}}
   return (
     <>
       <Header />
+      {Array.isArray(cards) &&
+        cards.map(
+          ({
+            value,
+            suit,
+            images: { png },
+          }: {
+            value: string;
+            suit: string;
+            images: { png: string };
+          }) => <img src={png} alt={`${value} ${suit}`} />
+        )}
       <div className="container">
         <div className="games-layout">
-          {getAllGames().map((x) => (
-            <Card
-              key={x.id}
-              className="game-card"
-              onClick={() => {
-                console.log('clicked');
-                history.push(`/game/${x.id}`);
-              }}
-            >
-              <CardActionArea>
-                <CardMedia
-                  className="game-card-media"
-                  image={x.image}
-                  title={x.name}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {x.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {x.description}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                    className="game-rating"
-                  >
-                    <strong>Rating:</strong> {x.rating}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          ))}
+          {Array.isArray(d2) &&
+            d2.map((g: any) => (
+              <Card
+                key={g.id}
+                className="game-card"
+                onClick={() => {
+                  console.log('clicked');
+                  history.push(`/game/${g.id}`);
+                }}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    className="game-card-media"
+                    image={g.image}
+                    title={g.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {g.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {g.description}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                      className="game-rating"
+                    >
+                      <strong>Rating:</strong> {g.rating}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))}
         </div>
       </div>
       <Route path="/game/:id" component={StoreFeatureGameDetail} />
